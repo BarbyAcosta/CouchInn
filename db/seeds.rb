ea47005fe6634ff21ff0737422e721e9e2227262
Gina <anshery@hotmail.com>
2015-12-09 13:58:53 -0300

    Subir a Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,14 +20,14 @@ u2=User.create(email: 'otro@live.com.ar', password: 'contraseña',  password_con
 u3=User.create(email: 'gina@live.com.ar', password: 'contraseña',  password_confirmation: 'contraseña',premium: 'false', admin: 'true')
 
 puts "cargo categoria de reservas"
-Couch.create(name:'Casa',description:"Hermosa casa con ubicacion excelente",imageurl:"https://dl.dropboxusercontent.com/u/34731102/casa.jpg" ,tipocouch_id:1, capacidad:5 ,localidad: "La Plata" , user: u1 )
-Couch.create(name:'Sillon',description:"Confortable sillon ",imageurl:"https://dl.dropboxusercontent.com/u/34731102/sillon.jpg", tipocouch_id:2, capacidad:3 ,localidad: "Neuquen", user: u1 )
-Couch.create(name:'Departamento',description:"Hermoso departamento, excelente vista",imageurl:"https://dl.dropboxusercontent.com/u/34731102/Departamento%20barato.jpg", tipocouch_id:3, capacidad:1 ,localidad: "Misiones" , user: u1 )
-Couch.create(name:'Habitacion',description:"Espaciosa habitacion",imageurl:"https://dl.dropboxusercontent.com/u/34731102/habitacion.jpg", tipocouch_id:4 , capacidad:2 ,localidad: "La Rioja" ,user: u1 )
-Couch.create(name:'Piso',description:"Amplia vivienda que consta de un piso",imageurl:"https://dl.dropboxusercontent.com/u/34731102/piso.jpg", tipocouch_id:5, capacidad:3 ,localidad: "Cordoba" ,user: u3 )
+c1=Couch.create(name:'Casa',description:"Hermosa casa con ubicacion excelente",imageurl:"https://dl.dropboxusercontent.com/u/34731102/casa.jpg" ,tipocouch_id:1, capacidad:5 ,localidad: "La Plata" , user: u1 )
+c2=Couch.create(name:'Sillon',description:"Confortable sillon ",imageurl:"https://dl.dropboxusercontent.com/u/34731102/sillon.jpg", tipocouch_id:2, capacidad:3 ,localidad: "Neuquen", user: u1 )
+c3=Couch.create(name:'Departamento',description:"Hermoso departamento, excelente vista",imageurl:"https://dl.dropboxusercontent.com/u/34731102/Departamento%20barato.jpg", tipocouch_id:3, capacidad:1 ,localidad: "Misiones" , user: u1 )
+c4=Couch.create(name:'Habitacion',description:"Espaciosa habitacion",imageurl:"https://dl.dropboxusercontent.com/u/34731102/habitacion.jpg", tipocouch_id:4 , capacidad:2 ,localidad: "La Rioja" ,user: u1 )
+c5=Couch.create(name:'Piso',description:"Amplia vivienda que consta de un piso",imageurl:"https://dl.dropboxusercontent.com/u/34731102/piso.jpg", tipocouch_id:5, capacidad:3 ,localidad: "Cordoba" ,user: u3 )
 
 puts "Cargo solicitudes de reserva"
-Reserve.create(name:'Casa',fechaini:'12/12/2016',fechafin:'20/12/2016', confirmada: 'false',couch_id: 1 , user: u1 )
-Reserve.create(name:'Sillon',fechaini:'1/12/2016',fechafin:'5/12/2016',  confirmada: 'false',couch_id: 2 , user: u2)
-Reserve.create(name:'Departamento',fechaini:'7/2/2016',fechafin:'9/2/2016' , confirmada: 'false',couch_id: 3 , user: u2)
+Reserve.create(name:'Casa',fechaini:'12/12/2016',fechafin:'20/12/2016', confirmada: 'false',couch: c1 , user: u1 )
+Reserve.create(name:'Sillon',fechaini:'1/12/2016',fechafin:'5/12/2016',  confirmada: 'false',couch: c2 , user: u2)
+Reserve.create(name:'Departamento',fechaini:'7/2/2016',fechafin:'9/2/2016' , confirmada: 'false',couch: c3 , user: u2)
 
